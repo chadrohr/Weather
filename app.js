c55b1398cbcb854e2c1d3ef378c0a85d5c7598fe
Chad Rohr <chadrohr@yahoo.com>
2016-10-12 20:28:58 -0600

    eod with delete

--- app.js
+++ app.js
@@ -1,36 +1,57 @@
 (function () {
- $('#close').on('click', function (e) {
-        e.preventDefault();
+//  $('#cityList').on('click', function (e) {
+//         e.preventDefault();
+//         for(var i=0; i<cityarr.length; i++){
+//             if()
+//         }
         
- )}
-    
+//  })
+ $( "#cityList" ).on( "click", ".close", function( event ) {
+    event.preventDefault();
+    // var cityname = $(this).parent().siblings(".panel-heading").next("span:first-child").text()
+    // console.log(cityname);
+    //console.log( $( this ).;
+    var zip = $(this).attr("id");
+    console.log(zip)
+    for(i=0; i< cityarr.length; i++){
+        var city = cityarr[i]
+        if(city.zipcode == zip){
+            cityarr.splice(i,1);
+        }   
+    }
+    update()
+});
+    var cityarr = [];
     $('#addCityBtn').on('click', function (e) {
         e.preventDefault();
         var zip = $('#newZipCode').val()
-        // console.log($('#newZipCode').val())
+         console.log($('#newZipCode').val())
     
         $('#newZipCode').val('');
 
         var city;
-
+        
         getCity(zip)
             .then(function (data) {
                 city = data;
+                console.log(city)
                 return getWeather(zip);
             })
             .then(function (weather) {
 
-                // console.log(weather);
+                console.log(weather);
+                var cityobject = {
+                    zipcode: city['post code'],
+                    cityname: city.places[0]['place name'],
+                    statename: city.places[0]['state abbreviation'],
+                    temper: parseInt(weather.main.temp),
+                    condition: weather.weather[0].description 
+                      
+                };
+                cityarr.push(cityobject);
+                update();
 
-                var template = $('#cityTemplate').html();
-
-                template = template
-                    .replace('{{ city }}', city.places[0]['place name'])
-                    .replace('{{ state }}', city.places[0]['state abbreviation'])
-                    .replace('{{ temp }}', parseInt(weather.main.temp))
-                    .replace('{{ conditions }}', weather.weather[0].description);
-
-                $('#cityList').append($(template));
+           
             },
             function (error) {
                 console.log('Zip is a bad one')
@@ -59,4 +80,20 @@
     
     // })
 })
+function update(){
+         $('#cityList').empty()
+        for(var i = 0; i < cityarr.length; i++){    
+            var city = cityarr[i] 
+            console.log(city)
+         var template = $('#cityTemplate').html();
+                template = template
+                    .replace('{{ city }}', city.cityname)
+                    .replace('{{ state }}', city.statename)
+                    .replace('{{ temp }}', city.temper)
+                    .replace('{{ conditions }}', city.condition)
+                    .replace('{{ zip code }}', city.zipcode)
+                $('#cityList').append($(template));
+        }
+}
+
 })()
